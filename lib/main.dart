e9350ff671f01a364f00da2fbd25565484ca7757
Charlesthehunter578945 <krlos7568@gmail.com>
2023-09-18 09:49:27 -0400

    Scaffold / yaml images

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          'Yellow Guorld',
-          style: TextStyle(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello appBar'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello body',
+            style: TextStyle(
+              fontSize: 24,
               color: Colors.blue,
-              decoration: TextDecoration.none,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              backgroundColor: Colors.black),
+            ),
+          ),
         ),
       ),
     ),
